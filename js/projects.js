afc8fc20313106767654ceef4c189560c2149abe
ianramzy <ianramzy@gmail.com>
2018-12-26 16:27:48 -0500

    Adding colour drop

--- js/projects.js
+++ js/projects.js
@@ -46,18 +46,6 @@ var projectPixelShooter =
     '</a>' +
     '</div>';
 
-var TestProject =
-    '<div class="project p-light" id="p-TestProject">' +
-    '<a class="overlay" href="https://ianramzy.github.io/projectPages/projectname.html" target="_blank">' +
-    '<div class="content vertical-center">' +
-    '<h4>Design &amp; Development</h4>' +
-    '<h1>projectname</h1>' +
-    '<p>projectsummary</p>' +
-    '<button>Explore <span class="arrow"></span></button>' +
-    '</div>' +
-    '</a>' +
-    '</div>';
-
 var TronPong =
     '<div class="project p-light" id="p-TronPong">' +
     '<a class="overlay" href="https://ianramzy.github.io/projectPages/TronPong.html" target="_blank">' +
@@ -70,51 +58,36 @@ var TronPong =
     '</a>' +
     '</div>';
 
+var ColourMatch =
+    '<div class="project p-dark" id="p-ColourMatch">' +
+    '<a class="overlay" href="https://ianramzy.github.io/projectPages/ColourMatch.html" target="_blank">' +
+    '<div class="content vertical-center">' +
+    '<h4>Designer &amp; Developer</h4>' +
+    '<h1>Colour Match</h1>' +
+    '<p>Simple and addicting flash game, where you match the colours in a rush against time!</p>' +
+    '<button>Explore <span class="arrow"></span></button>' +
+    '</div>' +
+    '</a>' +
+    '</div>';
 
-// var projectFlowww =
-// 	'<div class="project p-dark" id="p-flowww">' +
-// 	    '<a class="overlay" href="http://flowww.xyz/" target="_blank">' +
-// 	        '<div class="content vertical-center">' +
-// 	            '<h4>Design &amp; Development</h4>' +
-// 	            '<h1>Flowww</h1>' +
-// 	            '<p>Frustrated with many productivity tools that don\'t work for me, my friend and I built a beautiful Kanban board for task management.</p>' +
-// 	            '<button>Explore <span class="arrow"></span></button>' +
-// 	        '</div>' +
-// 	    '</a>' +
-// 	'</div>';
-//
-//
-//
-// var project6502 =
-// 	'<div class="project p-light" id="p-6502">' +
-// 	    '<a class="overlay" href="https://github.com/nimus0108/6502" target="_blank">' +
-// 	        '<div class="content vertical-center">' +
-// 	            '<h4>Hackathon: Design &amp; Frontend</h4>' +
-// 	            '<h1>6502 IDE</h1>' +
-// 	            '<p>At HackRPI, my friends and I built a solution to a problem we\'ve had while learning how to code in 6502 assembly language at school.</p>' +
-// 	            '<button>Explore <span class="arrow"></span></button>' +
-// 	        '</div>' +
-// 	    '</a>' +
-// 	'</div>';
-//
-// var projectSearch =
-// 	'<div class="project p-light" id="p-search">' +
-// 	    '<a class="overlay" href="https://github.com/nimus0108/Search" target="_blank">' +
-// 	        '<div class="content vertical-center">' +
-// 	            '<h4>Design &amp; Development</h4>' +
-// 	            '<h1>Search</h1>' +
-// 	            '<p>At HackRU, my friends and I improved Behance\'s search engine by using image recognition technology to search for the image itself rather than rely on tags.</p>' +
-// 	            '<button>Explore <span class="arrow"></span></button>' +
-// 	        '</div>' +
-// 	    '</a>' +
-// 	'</div>';
+var TestProject =
+    '<div class="project p-light" id="p-TestProject">' +
+    '<a class="overlay" href="https://ianramzy.github.io/projectPages/projectname.html" target="_blank">' +
+    '<div class="content vertical-center">' +
+    '<h4>Design &amp; Development</h4>' +
+    '<h1>projectname</h1>' +
+    '<p>projectsummary</p>' +
+    '<button>Explore <span class="arrow"></span></button>' +
+    '</div>' +
+    '</a>' +
+    '</div>';
+
 
 var featured = [  //order when clicked on matters
     projectTickeriQ,
     projectBubblesortDemonstration,
     projectPixelShooter,
     RecursiveTrees,
-    TestProject
 ];
 
 var all = [
@@ -129,7 +102,7 @@ var design = [
 
 var dev = [
     projectPixelShooter,
-    // project6502
+
 ];
 
 var hackathon = [
@@ -138,6 +111,7 @@ var hackathon = [
 
 var game = [
     projectPixelShooter,
+    ColourMatch,
     TronPong
 ];
 
